745b6d3cb8981438f1d4b20cdf2e822d1ed70f76
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-04-07 22:35:09 +0500

    :construction: Add separate by tag

--- src/news/views.py
+++ src/news/views.py
@@ -17,7 +17,7 @@ class NewsViewSet(
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
-    def retrieve(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs) -> Response:
         instance = self.get_object()
         instance.view.quantity += 1
         instance.view.save()
@@ -25,8 +25,20 @@ class NewsViewSet(
         return Response(serializer.data)
 
     @action(detail=True, methods=["GET"])
-    def stats(self, request: Request, *args, **kwargs):
+    def stats(self, request: Request, *args, **kwargs) -> Response:
         return super().retrieve(request, *args, **kwargs)
+
+    @action(detail=False, methods=["GET"], url_path=r"tags/(?P<slug>\w+)")
+    def tags(self, request: Request, slug: str, *args, **kwargs) -> Response:
+        queryset = self.filter_queryset(self.get_queryset()).filter(tags__slug=slug)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def get_serializer_class(self, *args, **kwargs):
         serializers = {"stats": NewsStatsSerializer}
